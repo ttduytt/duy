352f0f68fec871d6d999868c6dd3570ee82ab04f
Trương Trọng Duy <truongtrongduy1@gmail.com>
2023-11-03 21:53:19 +0700

    test

--- src/index.js
+++ src/index.js
@@ -22,12 +22,12 @@ app.use(express.json());
 app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views',
-path.join(__dirname, 'resources/views'));
+        path.join(__dirname, 'resources/views'));
 
-route   (app);
+route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+          console.log(`Example app listening on port ${port}`);
 });
 
 
